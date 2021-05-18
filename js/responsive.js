a7250788fa3d0c127321b86f5e849b74d9464283
Mustafa Shaikh <smustfa147@gmail.com>
2021-05-18 22:35:29 +0530

    Add animation to navbar

--- js/responsive.js
+++ js/responsive.js
@@ -7,16 +7,16 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
-        document.getElementsByTagName("header")[0].style.top = "0";
+        document.getElementsByClassName("navbar")[0].style.top = "0";
     } else {
-        document.getElementsByTagName("header")[0].style.top = "-200px";
+        document.getElementsByClassName("navbar")[0].style.top = "-200px";
     }
     prevScrollpos = currentScrollPos;
 }
 
 menu = document.querySelector('.menu');
-nav = document.querySelector('.header')
-navList = document.querySelector('.navbar')
+nav = document.querySelector('.navbar')
+navList = document.querySelector('.navList')
 
 
 function clicked() {
