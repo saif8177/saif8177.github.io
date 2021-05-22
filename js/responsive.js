337c7a8548342fb24fbfc2638973bd9f655ed556
Mustafa Shaikh <smustfa147@gmail.com>
2021-05-22 23:34:46 +0530

    Add current active tab in navbar

--- js/responsive.js
+++ js/responsive.js
@@ -1,29 +1,45 @@
+//  Init AOS
  AOS.init({
     duration: 1000
  });
 
+//  Set viewport height (fix for mobile)
 addEventListener("load", function() {
     var viewport = document.querySelector("meta[name=viewport]");
     viewport.setAttribute("content", viewport.content + ", height=" + window.innerHeight);
 })
 
+// Auto hide navbar on scroll
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
         document.getElementsByClassName("navbar")[0].style.top = "0";
     } else {
-        document.getElementsByClassName("navbar")[0].style.top = "-200px";
+        setTimeout(()=>{
+            document.getElementsByClassName("navbar")[0].style.top = "-250px";
+        },1500)
     }
     prevScrollpos = currentScrollPos;
 }
 
+// Hide Show navbar on web/mobile
 menu = document.querySelector('.menu');
 nav = document.querySelector('.navbar')
 navList = document.querySelector('.navList')
 
-
 function clicked() {
     nav.classList.toggle('h-nav');
     navList.classList.toggle('v-nav');
+}
+
+// Set active navitem
+var navItems = document.getElementsByClassName("nav-items");
+
+for (var i = 0; i < navItems.length; i++) {
+    navItems[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
 }
